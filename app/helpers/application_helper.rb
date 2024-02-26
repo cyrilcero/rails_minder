c46d9934ec63c93d5735bf5f98a80e9abe8e8f50
cyrilcero <cyril.cero@gmail.com>
2024-02-26 23:37:28 +0800

    feat: add helpers for formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_date(date)
+    date.strftime("%B #{date.day.ordinalize}, %Y")
+  end
+
+  def remove_domain_from_email(email)
+    email.gsub(/@.+$/, '')
+  end
 end
