fe8a54bf9c409aca0ffba0d2d738f6fe89b866db
cyrilcero <cyril.cero@gmail.com>
2024-02-27 00:43:54 +0800

    feat: set model validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
+  include TasksHelper
   belongs_to :category
 
   #validations
   validates :name, presence: true
-  validates :is_done, presence: true
   validates :target_completion_date, presence: true
 end
