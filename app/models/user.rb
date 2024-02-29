a47c79f0eaa7c7ac61825883be54218c4ec51511
cyrilcero <cyril.cero@gmail.com>
2024-03-01 02:29:35 +0800

    fix: revert devise user model to include rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :rememberable
 
   has_many :categories, dependent: :destroy
   has_many :tasks, through: :categories
