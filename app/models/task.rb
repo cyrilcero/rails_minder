9feec0c72be9a86500d897bf2ccbf6b000a6b19b
cyrilcero <cyril.cero@gmail.com>
2024-03-05 18:24:07 +0800

    fix: omit unnecessary method import

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,6 @@
 class Task < ApplicationRecord
-  include TasksHelper
   belongs_to :category
 
-  #validations
   validates :name, presence: true
   validates :target_completion_date, presence: true
 end
